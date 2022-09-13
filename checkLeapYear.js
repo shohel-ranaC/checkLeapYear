a7a499f047e39d296225e0dc76c96593cd4343e6
shohel-ranaC <akashshohel422@gmail.com>
2022-09-13 07:51:22 +0600

    check leap year or not

---
+++ checkLeapYear.js
@@ -0,0 +1,11 @@
+// const year = 2022;
+// const count = year%4;
+// console.log(count);
+
+const year = 2050;
+if(year%4 == 0){
+console.log("yes this year is a Leap year !");
+}
+else{
+    console.log("This Year is not Leap year !");
+}
